af57098cd15b4b71a38635f2dcb07542da0e9e66
Kyle B. Lackinger <67078189+kbl40@users.noreply.github.com>
2023-01-10 17:05:44 -0500

    updated routing configs

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import {defineConfig} from 'vite'
 
+const { resolve } = require('path')
+
+module.exports = {
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				about: resolve(__dirname, 'about/index.html'),
+				post: resolve(__dirname, 'post/index.html')
+			}
+		}
+	}
+}
 
 export default defineConfig({
 	plugins: []	
